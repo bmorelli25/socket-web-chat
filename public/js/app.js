59ceccea46c85e2ac8d65b6243ec0b7a699d05ba
pentel13 <bmorelli25@gmail.com>
2016-10-21 14:28:52 -0700

    Created a simple front end form/UI to submit message

--- public/js/app.js
+++ public/js/app.js
@@ -7,3 +7,18 @@ socket.on('connect', () => {
 socket.on('message', (message) => {
   console.log('New Message: ' + message.text);
 });
+
+// Handle submitting of new message
+var $form = jQuery('#message-form'); // use # to select by id
+
+$form.on('submit', (event) => { // built in browser event is called submit
+  event.preventDefault(); // stops a refresh of the page
+
+  var $message = $form.find('input[name=message]');
+
+  socket.emit('message', {
+    text: $message.val()
+  });
+
+  $message.val("");   // clear form field after submit
+});
