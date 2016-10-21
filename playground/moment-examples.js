c6079815772bf0018f4d4e4168b0dc4d3ede0964
pentel13 <bmorelli25@gmail.com>
2016-10-21 15:15:51 -0700

    playground - more moment fun

--- playground/moment-examples.js
+++ playground/moment-examples.js
@@ -2,10 +2,18 @@ var moment = require('moment');
 var now = moment();
 
 console.log(now.format());
-console.log(now.format(X));
+console.log(now.format('X')); // Unix time stamp
+console.log(now.format('x')); // JS Unix time stamp
+console.log(now.valueOf()); // Returns a number
 
 now.subtract(1, 'year');
 console.log(now.format());
 
 // now use a format string
 console.log(now.format("MMM Do YYYY, h:mma"));
+
+// ** REVERSE, REVERSE ** \\
+
+var timestamp = 1477087991555;
+var timestampMoment = moment.utc(timestamp);
+console.log(timestampMoment.local().format("h:mma"));
